a20b74815bef65e6e0f878fb0fefdd5567561f1f
abhisheksankhla121 <abhisheksankhla121@gmail.com>
2023-12-14 00:11:59 +0530

    froentend  context api done

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -17,7 +17,7 @@ export default function NoteItem(props){
     <p className="card-text">{notes.description} Card titles are used by adding .card-title to a  tag. In the same way, links are added and placed next to each other by adding .card-link to an  tag.
 Subtitles are used by adding a .card-subtitle to a  tag. If the .card-title and the .card-subtitle items are placed in a .card-body item, the card title and subtitle are aligned nicely </p>
    
-  </div>
+  </div>    
 </div>
 </div>
        </>
