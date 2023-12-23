bddb36be613658b916f37f952e73291b47ba97e4
abhisheksankhla121 <abhisheksankhla121@gmail.com>
2023-12-24 02:20:15 +0530

    backend updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongooseURI = "mongodb://127.0.0.1:27017/practice"
+const mongooseURI = "mongodb+srv://abhisheksankhla121:abhishek123@clusterabhishek.mnvmbsw.mongodb.net/"
 const connectTOMongo=()=>{
     try {
         mongoose.connect(mongooseURI)
